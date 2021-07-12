5cb3b054377760498dbf9d3cf503fba8b3d88c05
atulnk <atulnk1@gmail.com>
2021-07-12 20:52:16 +0800

    added gitignore

--- script.js
+++ script.js
@@ -1,35 +1,66 @@
 "user strict";
+import { createClient } from '@supabase/supabase-js'
 
-function geoFindMe() {
+const supabaseUrl = 'https://lzrvibrmfmuawvsxigbq.supabase.co'
+const supabaseKey = process.env.SUPABASE_KEY
+const supabase = createClient(supabaseUrl, supabaseKey)
 
-    const status = document.querySelector('#status');
-    const mapLink = document.querySelector('#map-link');
-  
-    mapLink.href = '';
-    mapLink.textContent = '';
-  
-    function success(position) {
-      const latitude  = position.coords.latitude;
-      const longitude = position.coords.longitude;
-  
-      status.textContent = '';
-      mapLink.href = `https://www.openstreetmap.org/#map=18/${latitude}/${longitude}`;
-      mapLink.textContent = `Latitude: ${latitude} °, Longitude: ${longitude} °`;
-    }
-  
-    function error() {
-      status.textContent = 'Unable to retrieve your location';
-    }
-  
-    if(!navigator.geolocation) {
-      status.textContent = 'Geolocation is not supported by your browser';
-    } else {
-      status.textContent = 'Locating…';
-      navigator.geolocation.getCurrentPosition(success, error);
-    }
+// function geoFindMe() {
 
-    const container = document.querySelector(".container");
+//     const status = document.querySelector('#status');
+//     const mapLink = document.querySelector('#map-link');
   
+//     mapLink.href = '';
+//     mapLink.textContent = '';
+  
+//     function success(position) {
+//       const latitude  = position.coords.latitude;
+//       const longitude = position.coords.longitude;
+  
+//       status.textContent = '';
+//       mapLink.href = `https://www.openstreetmap.org/#map=18/${latitude}/${longitude}`;
+//       mapLink.textContent = `Latitude: ${latitude} °, Longitude: ${longitude} °`;
+//     }
+  
+//     function error() {
+//       status.textContent = 'Unable to retrieve your location';
+//     }
+  
+//     if(!navigator.geolocation) {
+//       status.textContent = 'Geolocation is not supported by your browser';
+//     } else {
+//       status.textContent = 'Locating…';
+//       navigator.geolocation.getCurrentPosition(success, error);
+//     }
+
+//     const container = document.querySelector(".container");
+  
+// }
+
+
+function goFineMeSimple() {
+
+  const status = document.querySelector('#status');
+  function success(position) {
+    const latitude  = position.coords.latitude;
+    const longitude = position.coords.longitude;
+
+    status.textContent = `Latitude is: ${latitude}, Longitude is: ${longitude}`
+
+    console.log(`Latitude is: ${latitude}, Longitude is: ${longitude}`);
   }
-  
-  document.querySelector('#find-me').addEventListener('click', geoFindMe);
+
+  function error() {
+    status.textContent = 'Unable to retrieve your location';
+  }
+
+  if(!navigator.geolocation) {
+    status.textContent = 'Geolocation is not supported by your browser';
+  } else {
+    status.textContent = 'Locating…';
+    navigator.geolocation.getCurrentPosition(success, error);
+  }
+
+}
+
+document.querySelector('#find-me').addEventListener('click', goFineMeSimple);
